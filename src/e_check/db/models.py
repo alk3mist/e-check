f618d93eb613ea96d9dec9c3a7be8ff6e4c41ad2
Oleksandr Havrylin <oleksa.havrylin@outlook.com>
2025-04-27 15:47:06 +0300

    Create DB models and migration

---
+++ src/e_check/db/models.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+from decimal import Decimal
+from typing import Literal
+from uuid import UUID
+
+from sqlalchemy import Enum, ForeignKey, MetaData, String, Uuid, func, select
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    column_property,
+    mapped_column,
+    relationship,
+)
+
+meta = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    metadata = meta
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+
+    checks: Mapped["Check"] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Product(Base):
+    __tablename__ = "product"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    check_id: Mapped[int] = mapped_column(ForeignKey("check.id"))
+    name: Mapped[str] = mapped_column(String(255))
+    price: Mapped[Decimal] = mapped_column()
+    quantity: Mapped[Decimal] = mapped_column()
+
+    check: Mapped["Check"] = relationship(back_populates="products")
+
+    total: Mapped[Decimal] = column_property(name * price)
+
+
+class Payment(Base):
+    __tablename__ = "payment"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    check_id: Mapped[int] = mapped_column(ForeignKey("check.id"))
+    type: Mapped[Literal["cash", "cashless"]] = mapped_column(
+        Enum("cash", "cashless", name="payment_type_enum")
+    )
+    amount: Mapped[Decimal] = mapped_column()
+    check: Mapped["Check"] = relationship(back_populates="payment")
+
+
+class Check(Base):
+    __tablename__ = "check"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    external_id: Mapped[UUID] = mapped_column(Uuid(native_uuid=True), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(nullable=False)
+
+    user: Mapped[User] = relationship(back_populates="checks")
+    products: Mapped[list[Product]] = relationship(
+        back_populates="check", cascade="all, delete-orphan"
+    )
+    payment: Mapped[Payment] = relationship(
+        back_populates="check", cascade="all, delete-orphan"
+    )
+
+    total: Mapped["Decimal"] = column_property(
+        select(func.sum(Product.total))
+        .where(Product.check_id == id)
+        .correlate_except(Product)
+        .scalar_subquery()
+    )
