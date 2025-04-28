4cf2cf2777d8ee2a95c215b67ce659c156d9c066
Oleksandr Havrylin <oleksa.havrylin@outlook.com>
2025-04-28 17:53:12 +0300

    Fix path for settings.env_file so that project root is always the working directory

--- src/e_check/config.py
+++ src/e_check/config.py
@@ -5,8 +5,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        # Use top level .env file
-        env_file="../../.env",
+        # All commands must be run from the root project's directory,
+        # hence $PWD will be e-check
+        env_file=".env",
         env_ignore_empty=True,
         extra="ignore",
     )
